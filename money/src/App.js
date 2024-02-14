bbd0967f47965d9785097cff87b5019e43b674fd
harishm-git <harishdeepakh@gmail.com>
2024-02-14 21:48:35 +0530

    checking and testing

--- money/src/App.js
+++ money/src/App.js
@@ -1,27 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './header';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <p>comment</p> */}
-        <p>harish is a fullstack developer and till date he the highest paid developer in the world </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+       
+        <Header></Header>
+       
+        
   );
 }
 
---
+++ money/src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react.js'
+
+
+const header = () => {
+  return (
+    <div>header</div>
+  )
+}
+
+export default header
