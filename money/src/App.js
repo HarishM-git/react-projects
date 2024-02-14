18cb2df4ad10aac8da5c072a95b7311ae8f4c2f6
harishm-git <harishdeepakh@gmail.com>
2024-02-14 22:00:07 +0530

    just checking

--- money/src/App.js
+++ money/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './header';
+
 
 
 function App() {
   return (
    
        
-        <Header></Header>
+        <p>hello</p>
        
         
   );
