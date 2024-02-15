e485fd3f7f563dec230c9805b1f01854ffb5c900
harishm-git <harishdeepakh@gmail.com>
2024-02-15 21:46:12 +0530

    added some intresting scripts thats a paragraph about harish

--- money/src/Header.js
+++ money/src/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const header = () => {
   return (
-    <div>header</div>
+    <div>harish is a worlds best developer and an white hat hacker who is paid in billions dollers</div>
   )
 }
 
