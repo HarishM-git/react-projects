9809b5fa52384815d78999703770fd63390b9edc
harishm-git <harishdeepakh@gmail.com>
2024-02-15 21:33:41 +0530

    gaining more and more knowledge about how react works but it is just a beginning

--- money/src/App.js
+++ money/src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from "./Header.js";
 
 
 function App() {
   return (
-   
-       
-        <p>hello</p>
+      
+        <Header />
        
         
   );
