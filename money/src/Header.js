dfbbcdae7044117ef440b43a1532f51b7ae30c96
harishm-git <harishdeepakh@gmail.com>
2024-02-16 21:37:58 +0530

    again trying to deploy it in github

--- money/src/Header.js
+++ money/src/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const header = () => {
+const Header = () => {
   return (
     <div>harish is a worlds best developer and an white hat hacker who is paid in billions dollers</div>
   )
 }
 
-export default header
+export default Header
