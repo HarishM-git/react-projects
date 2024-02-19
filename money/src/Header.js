8cd2c02592cf3486a38375c78716874a2cbd5934
harishm-git <harishdeepakh@gmail.com>
2024-02-19 22:00:30 +0530

    added spaces

--- money/src/Header.js
+++ money/src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>harish is a worlds best developer and an white hat hacker who is paid in billion dollars</div>
+    <div>harish is a worlds best developer and an white hat hacker who is paid in billion  dollars</div>
   )
 }
 
