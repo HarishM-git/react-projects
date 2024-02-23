93455920ef6da6aa3d956fb1f5da62b1fc446c69
harishm-git <harishdeepakh@gmail.com>
2024-02-23 21:34:26 +0530

    learning use states

--- money/src/Main.js
+++ money/src/Main.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Main = () => {
+  function adida(){
+    setCount(count+1)
+  }
+  function odada(){
+    setCount(count-1)
+  }
+
+  const [count,setCount]=useState(0)
+
+
   return (
     <main>
+      
       <div><p>main</p></div>
+      <button onClick={adida}>+</button>
+      <div>{count}</div>
+      <button onClick={odada}>-</button>
     </main>
   )
 }
