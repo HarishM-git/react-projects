8bbde7cb2ded5073e548f03c4be6dba2ea44aa8c
harishm-git <harishdeepakh@gmail.com>
2024-03-04 21:28:45 +0530

    added react-icons and learning how to use it

--- money/src/Main.js
+++ money/src/Main.js
@@ -14,6 +14,28 @@ const Main = () => {
     })
   }
 */}
+
+
+const Content=()=>{
+  const [items,setItems]=useState([
+    {
+      id:1,
+      checked:true,
+      label:'practice coding'
+    },
+    {
+      id:1,
+      checked:true,
+      label:'eat HEALTHY'
+    },
+    {
+      id:1,
+      checked:false,
+      label:'go to GYM'
+    }
+  ])
+
+}
     
 
 
@@ -26,6 +48,18 @@ const Main = () => {
       {/*<div><p>main</p></div>
       <button onClick={change}>subscribe</button>
   <div> Harish is a {name}</div>*/}
+
+
+    <ul>
+      {items.map((item)=>{
+        <li>
+          <input type="checkbox" checked={item.checked} />
+          <label htmlFor="">{item.label}</label>
+          <button>delete</button>
+        </li>
+      })}
+    </ul>
+
     </main>
   )
 }
