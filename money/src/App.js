9004fb3767acd01029f952007831b78a7c89ce9f
harishm-git <harishdeepakh@gmail.com>
2024-03-04 22:10:11 +0530

    trying to fix the error

--- money/src/App.js
+++ money/src/App.js
@@ -1,17 +1,15 @@
 import './index.css'
 import Header from './Header';
-
 import Footer from './Footer';
-import Content from './Content';
 
 
-import bootstrap from 'bootstrap';
+
+
 
 
 function App() {
   return (
       <div className=''> <Header/>
-      <Content/>
       <Footer></Footer>
       </div>
        
